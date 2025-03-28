e4e45c41ba3a32145c7df1127c4d6e60a6b99bd3
kaan koc <kaankoc01@gmail.com>
2025-03-28 03:46:25 +0300

    Category İşlemleri Yapıldı

---
+++ ApiProjectKampi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,64 @@
+﻿using ApiProjectKampi.WebApi.Context;
+using ApiProjectKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Ok(_context.Categories);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByIdCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+            return Ok(category);
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            var updatedCategory = _context.Categories.Find(category.CategoryId);
+            if (updatedCategory == null)
+            {
+                return NotFound("Category not found");
+            }
+            updatedCategory.CategoryName = category.CategoryName;
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
