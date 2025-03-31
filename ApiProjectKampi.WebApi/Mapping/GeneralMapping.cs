8291be7f93df3e9d9df4f581db8a9340395e4e02
kaan koc <kaankoc01@gmail.com>
2025-03-31 09:00:23 +0300

    automapper işlemleri message ve feature controller yapıldı

---
+++ ApiProjectKampi.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjectKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjectKampi.WebApi.Dtos.MessageDtos;
+using ApiProjectKampi.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjectKampi.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
