0baa5262d7873afe8dbf9e6777ffff285c96daf5
kaan koc <kaankoc01@gmail.com>
2025-04-01 19:08:08 +0300

    fluent validation paketi kurulumu ve mantığı

---
+++ ApiProjectKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using ApiProjectKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjectKampi.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün adı boş geçilemez"); 
+            RuleFor(p => p.ProductName).MinimumLength(2).WithMessage("Ürün adı en az 2 karakter olmalıdır");
+            RuleFor(p => p.ProductName).MaximumLength(50).WithMessage("Ürün adı en fazla 50 karakter olmalıdır");
+
+            RuleFor(p => p.Price).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez")
+                .LessThan(0).WithMessage("Ürün fiyatı negatif olamaz")
+                .GreaterThan(1000).WithMessage("Ürün fiyatı Bu kadar yüksek olamaz girdiğiniz değeri kontrol ediniz.");
+
+            RuleFor(p => p.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez");
+        }
+    }
+}
